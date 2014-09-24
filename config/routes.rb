81e2fffa3b64159b418c365508a0a20fae0f7114
James Young <pronoiac@gmail.com>
2014-09-24 10:43:40 -0700

    Using navbar, replacing session partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  # get 'pages/home'
-  #
-  # get 'pages/about'
-  #
-  # get 'pages/contact'
-
+  
   resources :users
   resource  :session
   
   root to: 'pages#home'
-
+  get 'pages/about'
+  get 'pages/contact'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
