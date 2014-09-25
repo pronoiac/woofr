b085c58b9d2631f5b3243132cf14de3187ab4b30
James Young <pronoiac@gmail.com>
2014-09-25 14:17:54 -0700

    Adding images user_id index, upload link in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924185903) do
+ActiveRecord::Schema.define(version: 20140925203949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "user_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "images", ["user_id"], name: "index_images_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.text     "username",        null: false
