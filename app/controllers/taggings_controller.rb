47116136cce0502eef84e4b61819a823f2da774c
James Young <pronoiac@gmail.com>
2014-09-30 15:14:02 -0700

    Adding API and web interface for adding tags

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,5 +1,26 @@
 class TaggingsController < ApplicationController
   def create
+    tag_name = params[:tag][:name]
+    image_id = params[:tag][:image_id]
+    image_user_id = params[:tag][:image_user_id]
+    @tag = Tag.find_by(name: tag_name) || 
+      Tag.create!({name: tag_name})
+    tagging = Tagging.new ({
+      tag_id: @tag.id,
+      image_id: image_id
+    })
+    if tagging.save
+      # yay
+      flash[:notices] = ["Tag applied!"]
+    else
+      # nope
+      flash[:errors] = tagging.errors.full_messages
+      # redirect_to :back
+    end
+    redirect_to user_image_url(id: image_id, 
+      user_id: image_user_id)
+    
+    # fail
   end
 
   def index
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,7 @@
 class Tagging < ActiveRecord::Base
   
   validates :tag_id, :image_id, presence: true
+  validates_uniqueness_of :tag_id, :scope => [:image_id]
   
   belongs_to(
     :tag,
