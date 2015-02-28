a024164bf4be15374ca15c23ed866636c68efe3a
James Young <pronoiac@gmail.com>
2015-02-27 23:47:27 -0800

    Adding association, Tag has_many Taggings

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,13 @@
 class Tag < ActiveRecord::Base
   
   validates :name, presence: true
+
+  has_many(
+    :taggings,
+    class_name: "Tagging",
+    foreign_key: :tag_id,
+    primary_key: :id
+  )
+  
   
 end
